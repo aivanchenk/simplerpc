0b8bae45c352f168fd000b87cc1fe9be0b121469
Anastasiia <a.ivanchenko.eu@gmail.com>
2025-09-10 16:45:12 +0300

    add farm contract service

---
+++ FarmContract/IFarmService.cs
@@ -0,0 +1,30 @@
+﻿namespace FarmContract;
+
+/// <summary>
+/// Descriptor of submission result
+/// </summary>
+public class SubmissionResult
+{
+    /// <summary>
+    /// Indicates if submission attempt has accepted.
+    /// </summary>
+    public bool IsAccepted { get; set; }
+
+    /// <summary>
+    /// If pass submission has failed, indicates fail reason.
+    /// </summary>
+    public string FailReason { get; set; }
+}
+
+public enum FarmPhase
+{
+    Growing,
+    Selling,
+    Dead,        
+}
+
+public interface IFarmService
+{
+    SubmissionResult SubmitFood(int amount);
+    SubmissionResult SubmitWater(int amount);
+}
