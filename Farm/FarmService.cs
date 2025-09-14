1860c3272e8bcf164826d4177a0ea3129536081f
Anastasiia <a.ivanchenko.eu@gmail.com>
2025-09-14 23:08:05 +0300

    thread safe water accumulation

--- Farm/FarmService.cs
+++ Farm/FarmService.cs
@@ -7,9 +7,31 @@ using Services;
 /// </summary>
 public class FarmService : IFarmService
 {
-    public SubmissionResult SubmitFood(int amount)
-        => new SubmissionResult { IsAccepted = true, FailReason = string.Empty };
+
+    private static readonly object _waterLock = new();
+
+    private static int waterTotal = 0;
+    private static int foodTotal = 0;
 
     public SubmissionResult SubmitWater(int amount)
-        => new SubmissionResult { IsAccepted = true, FailReason = string.Empty };
+    {
+        lock (_waterLock)
+        {
+            waterTotal += amount;
+        }
+        Console.WriteLine($"Recieved {amount} of water resource. Now total water is {waterTotal}");
+        return new SubmissionResult { IsAccepted = true, FailReason = string.Empty };
+    }
+
+        public SubmissionResult SubmitFood(int amount)
+    {
+        lock (_waterLock)
+        {
+            foodTotal += amount;
+        }
+        Console.WriteLine($"Recieved {amount} of food resource. Now total food is {foodTotal}");
+        return new SubmissionResult { IsAccepted = true, FailReason = string.Empty };
+    }
+
+    
 }
