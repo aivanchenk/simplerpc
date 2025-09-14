d22454ef14a1d3b636a6665abb0539e1e43be9b4
Anastasiia <a.ivanchenko.eu@gmail.com>
2025-09-14 22:13:14 +0300

    start with Farm server

--- FarmContract/IFarmService.cs
+++ FarmContract/IFarmService.cs
@@ -16,15 +16,20 @@ public class SubmissionResult
     public string FailReason { get; set; }
 }
 
-public enum FarmPhase
-{
-    Growing,
-    Selling,
-    Dead,        
-}
 
 public interface IFarmService
 {
+    /// <summary>
+	/// Try submitting Food resource.
+	/// </summary>
+	/// <param name="amount">Amount of food submitted.</param>
+	/// <returns>Submit result descriptor.</returns>
     SubmissionResult SubmitFood(int amount);
+    
+    /// <summary>
+    /// Try submitting Water resource.
+    /// </summary>
+    /// <param name="amount">Amount of water submitted.</param>
+    /// <returns>Submit result descriptor.</returns>
     SubmissionResult SubmitWater(int amount);
 }
